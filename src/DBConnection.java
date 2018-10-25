d2af5a7263d633cbfe2638d83769ddc7aaa4d6cc
glazersh <glazersh@post.bgu.ac.il>
2018-10-25 16:21:03 +0300

    added maven and db

---
+++ src/DBConnection.java
@@ -0,0 +1,37 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+
+    private String ConnectionString = "jdbc:sqlite:resources/db/vacation4uDB.db";
+
+    Connection conn = null;
+    public DBConnection(){}
+
+    public Connection getSQLLiteDBConnection(){
+        if (conn == null) {
+            try {
+                // db parameters
+                // create a connection to the database
+                conn = DriverManager.getConnection(ConnectionString);
+                System.out.println("Connection to SQLite has been established.");
+
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            } finally {
+                try {
+                    if (conn != null) {
+                        conn.close();
+                    }
+                } catch (SQLException ex) {
+                    System.out.println(ex.getMessage());
+                }
+            }
+        }
+        return conn;
+    }
+
+
+}
