6597cbf1b068369f08c8b62bd5b28a5082464304
Danny <dannykoyfman@gmail.com>
2018-10-27 23:44:12 +0300

    changed

--- src/DeletePackage/DeleteController.java
+++ src/DeletePackage/DeleteController.java
@@ -5,8 +5,6 @@ package DeletePackage;
 import General.AController;
 import General.ModelUserDB;
 import interfaces.IPapaListener;
-import javafx.event.Event;
-import javafx.event.EventHandler;
 
 import java.util.ArrayList;
 
@@ -16,37 +14,54 @@ public class DeleteController extends AController {
 
     private static ArrayList<IPapaListener> lst;
 
+    public DeleteController(){};
+
     public DeleteController(ModelUserDB m_model, DeleteView m_view) {
         this.model = m_model;
         this.view = m_view;
 
-        this.view.setButtonCreateClickedHandler(new DeleteController.ButtonCreateClickedHandler());
+ //       this.view.setButtonCreateClickedHandler(new DeleteController.ButtonCreateClickedHandler());
 
         lst = new ArrayList<IPapaListener>();
     }
 
-    public void addPapaListener(IPapaListener papa){
-        if (papa != null && !lst.contains(papa))
-            lst.add(papa);
+    public void deleteUser(){
+
+        try {
+            model.delete(view.userTxt.getText());//need to add password
+        }
+        catch (Exception e){
+            view.showError();
+        }
+
     }
 
-    public DeleteController.ButtonCreateClickedHandler getButtonCreateClickedHandlerHandler(){
-        return new DeleteController.ButtonCreateClickedHandler();
-    }
 
+
+//    public void addPapaListener(IPapaListener papa){
+//        if (papa != null && !lst.contains(papa))
+//            lst.add(papa);
+//    }
+//
+//    public DeleteController.ButtonCreateClickedHandler getButtonCreateClickedHandlerHandler(){
+//        return new DeleteController.ButtonCreateClickedHandler();
+//    }
+//
     @Override
     public void back() {
 
     }
 
-    private class ButtonCreateClickedHandler implements EventHandler {
+//    private class ButtonCreateClickedHandler implements EventHandler {
+//
+//        @Override
+//        public void handle(Event event) {
+//
+//
+//
+//        }
+//
+//    }
 
-        @Override
-        public void handle(Event event) {
 
-
-
-        }
-
-    }
 }
--- src/DeletePackage/DeleteView.java
+++ src/DeletePackage/DeleteView.java
@@ -1,15 +1,13 @@
 package DeletePackage;
 
 import interfaces.IView;
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 
 import java.io.IOException;
 import java.util.Optional;
@@ -19,38 +17,66 @@ public class DeleteView implements IView {
     @FXML
     public Button b_delete;
     public Button b_back;
-public Label l_error;
+    public Label l_error;
     private Scene m_Scene;
+    public TextField userTxt;
+    public TextField passTxt;
+
+
+    private DeleteController m_controller;
 
     public DeleteView() {
+
         Parent root = null;
         try {
-            root = FXMLLoader.load(getClass().getResource("deleteFXML.fxml"));
+
+
+            FXMLLoader l=new FXMLLoader();
+            root = l.load(getClass().getResource("deleteFXML.fxml").openStream());
+            l.setController(this);
+
             this.m_Scene = new Scene(root);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
 
-        b_delete = (Button) root.lookup("#b_delete");
-        b_back=(Button) root.lookup("#b_back");
-        l_error=(Label)root.lookup("#l_error");
+
+//
+//
+//        b_delete = (Button) root.lookup("#b_delete");
+//        b_back=(Button) root.lookup("#b_back");
+//        l_error=(Label)root.lookup("#l_error");
     }
 
-    public void setButtonCreateClickedHandler(EventHandler buttonCreateEventHandler){
-        b_back.addEventHandler(javafx.scene.input.MouseEvent.MOUSE_CLICKED,buttonCreateEventHandler);
-    }
+
+
+    //
+//    public void setButtonCreateClickedHandler(EventHandler buttonCreateEventHandler){
+//        b_back.addEventHandler(javafx.scene.input.MouseEvent.MOUSE_CLICKED,buttonCreateEventHandler);
+//    }
     @Override
     public Scene getScene() {
         return this.m_Scene;
     }
 
-    public void handle(EventHandler e) {
+//    public void handle(EventHandler e) {
+//
+//        //model.read(username AND password)
+//        //if false-> show view.show l_error
+//        //else (below)
+//        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+//
+//        alert.setTitle("confirmation");
+//        alert.setHeaderText("Delete User");
+//        alert.setContentText("Are you sure?");
+//        Optional<ButtonType> result = alert.showAndWait();
+//        if (result.get() == ButtonType.OK)
+//            System.out.println("model.delete(username)");
+//    }
 
+    public void deleteUser(ActionEvent actionEvent) {
         l_error.setVisible(false);
-        //model.read(username AND password)
-        //if false-> show view.show l_error
-        //else (below)
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
 
         alert.setTitle("confirmation");
@@ -58,6 +84,12 @@ public Label l_error;
         alert.setContentText("Are you sure?");
         Optional<ButtonType> result = alert.showAndWait();
         if (result.get() == ButtonType.OK)
-            System.out.println("model.delete(username)");
+            m_controller.deleteUser();
     }
+
+    public void showError(){
+        l_error.setVisible(true);
+    }
+
+
 }
