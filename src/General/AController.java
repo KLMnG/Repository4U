57ffeae9f092c82078bad532d168ca66e1ed8e24
Danny <dannykoyfman@gmail.com>
2018-10-27 13:58:16 +0300

    added func to AController

--- src/General/AController.java
+++ src/General/AController.java
@@ -1,9 +1,22 @@
 package General;
 
 
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+
+
 public abstract class AController {
 
-
+private Button b_back;
 public  abstract void back();
+
+
+//    public AController(Button b_back) {
+//        this.b_back = b_back;
+//    }
+
+    public void setButtonCreateClickedHandler(EventHandler buttonCreateEventHandler){
+        b_back.addEventHandler(javafx.scene.input.MouseEvent.MOUSE_CLICKED,buttonCreateEventHandler);
+    }
 
 }
