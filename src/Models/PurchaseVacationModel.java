d2a0d875a713b1c228a172c2bdcfa2f19822ce88
dorlev <dor6688@gmail.com>
2018-12-11 00:57:06 +0200

    PurchaseVacationModel+Controller+View

--- src/Models/PurchaseVacationModel.java
+++ src/Models/PurchaseVacationModel.java
@@ -1,13 +1,14 @@
 package Models;
 
 import General.DBConnection;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Optional;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class PurchaseVacationModel {
 
@@ -17,9 +18,10 @@ public class PurchaseVacationModel {
         con = new DBConnection();
     }
 
-    public void addPurchaseVacation(String code_ticket, String seller){
 
-        String userNameSeller = seller;
+
+    public void addPurchaseVacation(String code_ticket, String userNameSeller){
+
         String userNameBuyer = UserModel.getUsername();
 
         String sql = "INSERT INTO RequestPurchase(seller,buyer,code_vacation,confirm_seller,confirm_buyer) Values(?,?,?,?,?)";
@@ -38,36 +40,122 @@ public class PurchaseVacationModel {
         }
     }
 
-    public void confirmVacation(String seller, String buyer, String code_vacation){
-        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-        alert.setTitle("Confirmation");
-        alert.setHeaderText("Confirm vacation");
-        alert.setContentText("Are you sure you want to sell?");
+    public Map<String,String> listOfBuyers(String seller){
+        Map<String,String> buyers = null;
+        String sql = "SELECT buyer,code_vacation"
+                + "FROM RequestPurchase WHERE seller = ? ";
 
-        Optional<ButtonType> result = alert.showAndWait();
-        if (result.get() == ButtonType.OK){
-            confirmVacationInDB(seller,buyer,code_vacation);
-        } else {
-            // ... user chose CANCEL or closed the dialog
+        try (Connection conn = con.getSQLLiteDBConnection();
+             PreparedStatement pstmt  = conn.prepareStatement(sql)){
+
+            pstmt.setString(1,seller);
+            ResultSet rs  = pstmt.executeQuery();
+
+            while (rs.next()) {
+                String buyer = (rs.getString("buyer"));
+                String code =  (rs.getString("code_vacation"));
+                buyers.put(code,buyer);
+            }
+        } catch (SQLException e) {
         }
+        // now we have map with all the codeVacation and his buyers
+        return buyers;
     }
 
-    private void confirmVacationInDB(String seller, String buyer, String code_vacation){
+    public void confirmVacationInDB(String seller, String buyer, String code_ticket){
         String sql = "UPDATE RequestPurchase SET confirm_seller = ? "+
                 "WHERE seller = ? , buyer = ? , code_vacation = ?";;
 
         try (Connection conn = con.getSQLLiteDBConnection();
              PreparedStatement pstmt = conn.prepareStatement(sql)) {
 
-            // set the corresponding param
             pstmt.setInt(1, 1);
             pstmt.setString(2, seller);
             pstmt.setString(3, buyer);
-            pstmt.setString(4, code_vacation);
-            // update
+            pstmt.setString(4, code_ticket);
+
             pstmt.executeUpdate();
 
         } catch (SQLException e) {
         }
     }
+
+    public boolean confirmation(String seller, String buyer){
+
+        List <Integer> arrl = null;
+
+        String sql = "SELECT confirm_seller,confirm_buyer"
+                + "FROM RequestPurchase WHERE seller = ?, buyer = ?";
+
+        try (Connection conn = con.getSQLLiteDBConnection();
+             PreparedStatement pstmt  = conn.prepareStatement(sql)){
+
+            pstmt.setString(1,seller);
+            pstmt.setString(2,buyer);
+
+            ResultSet rs  = pstmt.executeQuery();
+
+            arrl = new ArrayList();
+
+            arrl.add(rs.getInt("confirm_seller"));
+            arrl.add(rs.getInt("confirm_buyer"));
+
+        } catch (SQLException e) {
+        }
+        if(arrl == null)
+            return false;
+        if(arrl.get(0) == 1 && arrl.get(1) == 1)
+            return true;
+        return false;
+    }
+
+
+
+    public List<String> read(String code_ticket, String code_luggages, String code_vacation, String code_hotel) {
+        List<String> vacationInfo = null;
+        String sql = "SELECT Tickets.code, flight_company,departure_date," +
+                "departure_from, destination, ticket_type, seller, " +
+                "Luggages.weight,Luggages.height,Luggages.width" +
+                ",Vacations.time_to_stay, Vacations.vacation_type, " +
+                ",Hotels.address, Hotels.rate, Price "
+                + "FROM Tickets, Luggages, Vacation, Hotels "+
+                "WHERE Tickets.code = ?, Luggages.code = ?, Vacation.code = ?, Hotel.code = ?";
+
+        try (Connection conn = con.getSQLLiteDBConnection();
+             PreparedStatement pstmt  = conn.prepareStatement(sql)){
+
+            pstmt.setString(1,code_ticket);
+            pstmt.setString(2,code_luggages);
+            pstmt.setString(2,code_vacation);
+            pstmt.setString(2,code_hotel);
+
+            ResultSet rs  = pstmt.executeQuery();
+
+            vacationInfo = new ArrayList<>();
+
+            while (rs.next()) {
+                vacationInfo.add(rs.getString("Tickets.code"));
+                vacationInfo.add(rs.getString("flight_company"));
+                vacationInfo.add(rs.getString("departure_date"));
+                vacationInfo.add(rs.getString("departure_from"));
+                vacationInfo.add(rs.getString("destination"));
+                vacationInfo.add(rs.getString("ticket_type"));
+                vacationInfo.add(rs.getString("seller"));
+                vacationInfo.add(rs.getString("weight"));
+                vacationInfo.add(rs.getString("height"));
+                vacationInfo.add(rs.getString("width"));
+                //tmp.add(rs.getString(CounntTravelers()+""));
+                vacationInfo.add(rs.getString("time_to_stay"));
+                vacationInfo.add(rs.getString("vacation_type"));
+                vacationInfo.add(rs.getString("address"));
+                vacationInfo.add(rs.getString("rate"));
+                vacationInfo.add(rs.getString("Price"));
+            }
+        } catch (SQLException e) {
+        }
+        return vacationInfo;
+    }
+
+
+
 }
