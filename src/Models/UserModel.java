3f90afe4aca158c67bfd3cac0aadc1a32e5db0a9
koyfdan <@df>
2019-01-03 14:56:25 +0200

    update for exchange function UserModel : 1. getMyVacations
    VacationModel :
    1. getAllVacationByType
    2. setNewStateForVacation
    3. changeOwnersVacation
    4. changeOwnerVacation

--- src/Models/UserModel.java
+++ src/Models/UserModel.java
@@ -1,14 +1,15 @@
 package Models;
 
-import General.DBConnection;
-import General.User;
+import General.*;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class UserModel {
 
@@ -130,6 +131,53 @@ public class UserModel {
         }
         return arrl;
     }
+
+    public List<VacationData> getMyVacation(String user, String state){
+        Map<Integer, VacationData> vacations = new HashMap<>();
+        List<VacationData> allVacation= new ArrayList<>();;
+        VacationData vacationD;
+        String sql = "SELECT Tickets.code as ticketCode, flight_company ,departure_date, includes_flight_back, departure_from, destination, ticket_type, weight, height, width, \n" +
+                "Users.Username as Username, Users.Password as Password, Users.BirthDate as BirthDate,Users.FirstName as FirstName, Users.LastName as LastName, Users.City as City,\n" +
+                "Vacations.code as VacationCode, Vacations.time_to_stay as time_to_stay, Vacations.vacation_type as vacation_type, Vacations.hotel as hotel, Vacations.owner as owner, Vacations.price as price, Vacations.state as state, \n" +
+                "Hotels.code as hotelName ,Hotels.address as address, Hotels.rate as rate\n" +
+                "FROM Tickets\n" +
+                "left join Vacations on Vacations.code = Tickets.vacation\n" +
+                "left join Hotels on Hotels.code = Vacations.hotel\n" +
+                "left join Users on Vacations.owner = Users.Username\n" +
+                "WHERE Vacations.owner = ? AND Vacation.state = ?";
+
+        try (Connection conn = con.getSQLLiteDBConnection();
+             PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1,user);
+            pstmt.setString(2,state);
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+                User seller = new User(rs.getString("Username"), "Password", "FirstName", "LastName", "City", "BirthDate");
+                String vacationType = rs.getString("vacation_type");
+                int price = rs.getInt("price");
+
+                TicketData ticketData = new TicketData(rs.getString("ticketCode"), rs.getString("departure_from"), rs.getString("destination"),
+                        rs.getString("departure_date"), rs.getString("flight_company"), rs.getInt("weight"),
+                        rs.getInt("height"),rs.getInt("width"), rs.getString("ticket_type"),
+                        rs.getInt("includes_flight_back"),rs.getInt("VacationCode"));
+
+                if (!vacations.containsKey(rs.getInt("VacationCode"))) {
+                    List<TicketData> lst = new ArrayList<>();
+                    lst.add(ticketData);
+                    HotelData hotelData = new HotelData(rs.getString("hotelName"),rs.getString("address"),rs.getString("rate"));
+                    vacationD = new VacationData(lst,price, vacationType, rs.getInt("VacationCode"), rs.getInt("time_to_stay"), seller,hotelData);
+                    allVacation.add(vacationD);
+                    vacations.put(rs.getInt("VacationCode"), vacationD);
+                } else vacations.get(rs.getInt("VacationCode")).addToTicketData(ticketData);
+
+            }
+
+
+        } catch (SQLException e) {
+        }
+        return allVacation;
+    }
+
     public boolean isUserExists(String Username){
         List result = read(Username);
         return (result != null && result.size() > 0);
